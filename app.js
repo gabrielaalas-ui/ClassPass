7e537a34678c293655eb9e074861f97fcdab3308
gabrielaalas-ui <gabriela.alas@clevelandisd.org>
2026-07-20 20:00:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,67 @@
+let selectedDestination = "";
+
+
 function selectDestination(destination) {
+
+
+    selectedDestination = destination;
 
 
     document.getElementById("message").innerHTML =
 
-    "Selected: " + destination;
+    "Ready to scan for: " + destination;
+
+
+    startScanner();
+
+
+}
+
+
+
+function startScanner(){
+
+
+    let scanner = new Html5Qrcode("reader");
+
+
+
+    scanner.start(
+
+        {
+            facingMode: "environment"
+        },
+
+
+        {
+            fps: 10,
+            qrbox: 250
+        },
+
+
+        function(decodedText){
+
+
+            scanner.stop();
+
+
+
+            document.getElementById("message").innerHTML =
+
+            "QR Code Found: " + decodedText;
+
+
+
+        },
+
+
+        function(errorMessage){
+
+            // Ignore scanning errors while searching
+
+        }
+
+    );
 
 
 }
